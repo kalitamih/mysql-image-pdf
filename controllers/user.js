216d573f8e3173a7f01c3ce4d00c02946267c93b
Kalita Mikhail <mkalita1991@gmail.com>
2019-06-21 04:36:30 +0300

    fix: remove temporary created pdf-file

--- controllers/user.js
+++ controllers/user.js
@@ -2,6 +2,44 @@ const PDFDocument = require('pdfkit');
 const fs = require('fs');
 const User = require('../models/user');
 
+const removeFile = (id) => {
+  fs.unlink(`./public/pdf/${id}.pdf`, (err) => {
+    if (err) {
+      console.log(`File with id:${id} was not removed. ${err}`);
+    } else {
+      console.log(`File with id:${id} was removed`);
+    }
+  });
+};
+
+const createPDF = (writeStream, image, text) => {
+  const doc = new PDFDocument();
+  doc.pipe(writeStream);
+  doc.text(text, {
+    width: 410,
+    align: 'left',
+  });
+  doc.image(image, 50, 150, { width: 300 });
+  doc.end();
+};
+
+const savePDF = (id) => {
+  const readableStream = fs.readFileSync(`./public/pdf/${id}.pdf`);
+  User
+    .update({ pdf: readableStream }, {
+      where: {
+        id,
+      },
+    })
+    .then(() => {
+      console.log(`PDF-file for record with id: ${id} was created`);
+      removeFile(id);
+    })
+    .catch((err) => {
+      console.log(`PDF-file for record with id: ${id} was not created. ${err}`);
+    });
+};
+
 exports.postDisplayUsers = (req, res) => {
   const { user } = req.body;
   User
@@ -9,33 +47,16 @@ exports.postDisplayUsers = (req, res) => {
     .then((users) => {
       const noPdfUser = users.filter(item => !item.pdf);
       if (noPdfUser.length) {
-        const {
-          firstName, lastName, image, id,
-        } = noPdfUser[0];
-        const text = `${firstName} ${lastName}`;
-        const doc = new PDFDocument();
-        const writeStream = fs.createWriteStream(`./public/pdf/${id}.pdf`);
-        doc.pipe(writeStream);
-        doc.text(text, {
-          width: 410,
-          align: 'left',
-        });
-        doc.image(image, 50, 150, { width: 300 });
-        doc.end();
-        writeStream.on('finish', () => {
-          const readableStream = fs.readFileSync(`./public/pdf/${id}.pdf`);
-          User
-            .update({ pdf: readableStream }, {
-              where: {
-                id,
-              },
-            })
-            .then(() => {
-              console.log(`PDF-file for record with id: ${id} was created`);
-            })
-            .catch((err) => {
-              console.log(`PDF-file for record with id: ${id} was not created. ${err}`);
-            });
+        noPdfUser.forEach((item) => {
+          const {
+            firstName, lastName, image, id,
+          } = item;
+          const text = `${firstName} ${lastName}`;
+          const writeStream = fs.createWriteStream(`./public/pdf/${id}.pdf`);
+          createPDF(writeStream, image, text);
+          writeStream.on('finish', () => {
+            savePDF(id);
+          });
         });
         res.render('users');
       } else {
